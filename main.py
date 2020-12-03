71644297ed207a06e60bbe4fbc8beed826a68908
Thu Aung <satannatsoe23@gmail.com>
2020-12-03 07:29:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This program is a simple Mad Lib Generator.
+# Written by: Thu Aung
+# Written on: Sept 12, 2020.
+
+thing = input('What is the first thing you do after waking up? ')
+color = input('What is your favorite color? ')
+number = input('What is your favorite number? ')
+animal = input('What animal you most afraid of? ')
+love = input('Who is your first love? ')
+city = input('Where do you live? ')
+game = input('What is your favorite game? ')
+name = input('What do you want to be called? ')
+hobby = input('What is your hobbby? ')
+
+
+print(f'If you can {thing} and not make {color} your master;')
+print(f'If you can {animal} and not make {number} your aim;')
+print(f'If you can meet with {game} and {name}')
+print(f'And treat those two {hobby} just the same;')
+print(f'If you can bear to {thing} the truth you’ve {city}')
+print(f'Twisted by {animal} to make a trap for {love},')
+print(f'Or watch the {color} you gave your {game} to, broken,')
+print(f'And {thing} and {number} ’em up with {hobby} tools:')
